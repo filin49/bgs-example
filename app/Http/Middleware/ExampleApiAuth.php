e07a88b8fae3c30c19c5c698bd6f29dde12c4ff9
Alexandr Dudov <filin4949@gmail.com>
2020-07-09 17:53:02 +0300

    Add example API Auth for tests

---
+++ app/Http/Middleware/ExampleApiAuth.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class ExampleApiAuth
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @param  string|null  $guard
+     * @return mixed
+     */
+    public function handle($request, Closure $next, $guard = null)
+    {
+//        if ($request->header('Bearer') !== '123') {
+//            return response()->json(['status' => 'error', 'message' => 'Unauthorized'], 401);
+//        }
+
+        return $next($request);
+    }
+}
